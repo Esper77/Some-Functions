d6624254842c3cf2dadc0b269c88df25ee050c4e
Esper77 <112155793+Esper77@users.noreply.github.com>
2022-11-20 21:04:18 +0300

    Update e_pack2.cpp

--- e_pack2.cpp
+++ e_pack2.cpp
@@ -8,25 +8,25 @@ bool itc_iseven(int num){
 }
 
 int itc_min(int a, int b){
-    if (a < b);
+    if (a < b)
         return a;
     return b;
 }
 
 int itc_max(int a, int b){
-    if (a > b);
+    if (a > b)
         return a;
     return b;
 }
 
 double itc_fmin(double a, double b){
-    if (a < b);
+    if (a < b)
         return a;
     return b;
 }
 
 double itc_fmax(double a, double b){
-    if (a > b);
+    if (a > b)
         return a;
     return b;
 }
