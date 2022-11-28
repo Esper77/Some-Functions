9e309635a6f0381ddc6e69c9fd59761670e65114
Esper77 <112155793+Esper77@users.noreply.github.com>
2022-11-28 13:02:36 +0300

    Update m_pack3.cpp

--- m_pack3.cpp
+++ m_pack3.cpp
@@ -4,42 +4,28 @@
 using namespace std;
 
 int itc_second_max_num(long long number) {
+	int max = itc_max_num(number);
 	int max2 = 0;
-	int max1 = itc_max_num(number);
 	if (number < 0) {
-		number *= -1;
+		number = -number;
 	}
 	if (number < 10) {
 		return -1;
 	}
 	while (number > 0) {
-		if (number % 10 > max2 && number % 10 != max1) {
+		if (max2 < number % 10 && number % 10 != max)
 			max2 = number % 10;
-		}
-		number /= 10;
+		if (number % 10 == max)
+			max = 10;
+		number = number / 10;
 	}
 	return max2;
 }
 
 int itc_second_simple_max_num(long long number) {
-	int max2 = 0;
-	int max1 = itc_max_num(number);
-	if (number < 10) {
+	if (itc_second_max_num(number) == itc_max_num(number))
 		return -1;
-	}
-	if (number < 0) {
-		number *= -1;
-	}
-	while (number > 0) {
-		if (number % 10 > max2 && number % 10 != max1) {
-			max2 = number % 10;
-		}
-		number /= 10;
-	}
-	if (max2 == 0) {
-		return -1;
-	}
-	return max2;
+	return itc_second_max_num(number);
 }
 
 long long itc_bin_num(long long number) {
